423545b1e269658e0cfa43b37058597f98619111
Sergio Ferrer <sergiofrrg@gmail.com>
2021-12-07 17:27:01 +0000

    fix: popup toggle turned into open and hide

--- scripts/index.js
+++ scripts/index.js
@@ -10,19 +10,23 @@ const editButton = profile.querySelector(".profile__edit-button");
 const profileName = profile.querySelector('.profile__name');
 const profileDescription = profile.querySelector('.profile__description');
 
+function openPopUp(){
+  nameInput.value = profileName.textContent;
+  descriptionInput.value = profileDescription.textContent;
+  popup.classList.add("popup_opened");
+}
 
-
-function togglePopUp(){
-  popup.classList.toggle("popup_opened");
+function hidePopUp(){
+  popup.classList.remove("popup_opened");
 }
 
 function handleProfileFormSubmit(evt){
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileDescription.textContent = descriptionInput.value;
-  togglePopUp();
+  hidePopUp();
 }
 
-editButton.addEventListener("click", togglePopUp);
-closeButton.addEventListener("click", togglePopUp);
+editButton.addEventListener("click", openPopUp);
+closeButton.addEventListener("click", hidePopUp);
 form.addEventListener("submit", handleProfileFormSubmit);
