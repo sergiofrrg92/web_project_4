03687147a2b1e828b6ca8e8895f840880fd0ed2c
Sergio Ferrer Gimeno <sergiofrrg@tokenadmins-MacBook-Pro.local>
2021-12-07 09:31:13 +0000

    feature: name and desc edition

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,29 @@
-let popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup');
+const closeButton = popup.querySelector('.popup__close-button');
+
+const form = popup.querySelector('.form');
+const nameInput = form.querySelector('.form__text-input[name="name"]');
+const descriptionInput = form.querySelector('.form__text-input[name="about-me"]');
+const saveButton = form.querySelector('.form__save-button');
+
+const profile = document.querySelector('.profile');
+const editButton = profile.querySelector(".profile__edit-button");
+const profileName = profile.querySelector('.profile__name');
+const profileDescription = profile.querySelector('.profile__description');
+
+
 
 function togglePopUp(){
   popup.classList.toggle("popup_opened");
 }
 
-let editButton = document.querySelector(".profile__edit-button");
-let closeButton = document.querySelector(".popup__close-button");
+function handleProfileFormSubmit(evt){
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  togglePopUp();
+}
+
 editButton.addEventListener("click", togglePopUp);
 closeButton.addEventListener("click", togglePopUp);
+form.addEventListener("submit", handleProfileFormSubmit);
