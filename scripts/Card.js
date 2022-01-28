38ef74468f759f00a17fddad848666a062dbbcc8
Sergio Ferrer <73060595+sergiofrrg92@users.noreply.github.com>
2022-01-28 21:47:15 +0000

    more changes

--- scripts/Card.js
+++ scripts/Card.js
@@ -2,5 +2,60 @@ class Card {
     constructor(cardData, cardSelector) {
         this._text = cardData.text;
         this._image = cardData.image;
+        this._cardSelector = cardSelector;
     }
+
+        /**
+     * Creates one single photo card with specified card information (object)
+     * @param {object} card
+     * @returns photoCard to be rendered
+     */
+    _createCard(card) {
+        const photoCard = photoCardTemplate.querySelector(".photo-card").cloneNode(true);
+    
+        const photo = photoCard.querySelector('.photo-card__photo');
+        const photoTitle = photoCard.querySelector(".photo-card__title");
+    
+        photo.src = card.link;
+        photo.alt = card.name;
+    
+        photoTitle.textContent = card.name;
+    
+        const likeButton = photoCard.querySelector(".photo-card__like-button");
+        addLikeEventListener(likeButton);
+    
+        const deleteButton = photoCard.querySelector(".photo-card__delete-button");
+        addDeleteEventListener(deleteButton);
+    
+        addOpenPhotoEventListener(photo);
+    
+        return photoCard;
+    
+    }
+
+    addLikeEventListener(likeButton) {
+        likeButton.addEventListener("click", handleLikeEvent);
+    }
+
+    addDeleteEventListener(deleteButton) {
+        deleteButton.addEventListener("click", handleDeletePhotoEvent);
+    }
+
+    addOpenPhotoEventListener(photo) {
+        photo.addEventListener("click", (evt) => {
+          handleOpenPhotoEvent(evt);
+          openPopUp(popupPhoto);
+        });
+      }
+    
+    /**
+     * Renders the card created by createCard()
+     * @param {string} name - The title of the card
+     * @param {string} link - The link of the photo
+     */
+    renderCard(card) {
+        const photoCard = this._createCard(card);
+        photoGrid.prepend(photoCard);
+    }
+
 }
