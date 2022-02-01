71f79a38f597e0286ee3e4cd4573e0b5db616a2c
Sergio Ferrer <73060595+sergiofrrg92@users.noreply.github.com>
2022-02-01 21:09:32 +0000

    fix: card and part of index, formValidation to go

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,12 +1,14 @@
 import {openPopUp} from "./utils.js"
 
+const popupPhoto = '.popup-photo';
+
 class Card {
 
     constructor(cardData, cardSelector) {
-        this._name = cardData.name;
-        this._image = cardData.link;
+
+        [this._name, this._image] = [cardData.name, cardData.link];
         this._cardSelector = cardSelector;
-        this._popupPhoto = document.querySelector('.popup-photo');
+        this._popupPhoto = document.querySelector(popupPhoto);
     }
 
         /**
@@ -15,6 +17,27 @@ class Card {
      * @returns photoCard to be rendered
      */
     createCard() {
+        const photoCard = this._createNewPhotoCardElement();
+        this._addEventListeners(photoCard);
+        this._element = photoCard;
+    
+        return photoCard;
+    
+    }
+
+    _addEventListeners(photoCard){
+
+        const likeButton = photoCard.querySelector(".photo-card__like-button");
+        this._addLikeEventListener(likeButton);
+    
+        const deleteButton = photoCard.querySelector(".photo-card__delete-button");
+        this._addDeleteEventListener(deleteButton);
+    
+        const photo = photoCard.querySelector('.photo-card__photo');
+        this._addOpenPhotoEventListener(photo);
+    }
+
+    _createNewPhotoCardElement(){
         const photoCard = this._cardSelector.querySelector(".photo-card").cloneNode(true);
     
         const photo = photoCard.querySelector('.photo-card__photo');
@@ -23,34 +46,21 @@ class Card {
         photo.src = this._image;
         photo.alt = this._name;
         photoTitle.textContent = this._name;
-    
-        const likeButton = photoCard.querySelector(".photo-card__like-button");
-        this._addLikeEventListener(likeButton);
-    
-        const deleteButton = photoCard.querySelector(".photo-card__delete-button");
-        this._addDeleteEventListener(deleteButton);
-    
-        this._addOpenPhotoEventListener(photo);
-    
+
         return photoCard;
-    
     }
 
     _addLikeEventListener(likeButton) {
-        likeButton.addEventListener("click", this._handleLikeEvent);
+        likeButton.addEventListener("click", () => {
+            this._element.querySelector('.photo-card__like-button').classList.toggle('photo-card__like-button_active');
+        });
     }
-
-    _handleLikeEvent(evt) {
-        evt.target.classList.toggle('photo-card__like-button_active');
-      }
 
     _addDeleteEventListener(deleteButton) {
-        deleteButton.addEventListener("click", this._handleDeletePhotoEvent);
+        deleteButton.addEventListener("click", () => {
+            this._element.remove();
+        });
     }
-
-    _handleDeletePhotoEvent(evt) {
-        evt.target.closest('.photo-card').remove();
-      }
 
     _addOpenPhotoEventListener(photo) {
         photo.addEventListener("click", (evt) => {
@@ -59,15 +69,15 @@ class Card {
         });
     }
 
-    _handleOpenPhotoEvent(evt) {
-        const photoCard = evt.target.closest('.photo-card');
+    _handleOpenPhotoEvent() {
+        const photoCard = this._element;
         const photoContainer = this._popupPhoto.querySelector('.popup-photo__container');
         const photo = photoContainer.querySelector('.popup-photo__photo');
         const title = photoContainer.querySelector('.popup-photo__title');
       
-        photo.src = photoCard.querySelector('.photo-card__photo').src;
-        title.textContent = photoCard.querySelector('.photo-card__title').textContent;
-        photo.alt = photoCard.querySelector('.photo-card__title').textContent;
+        photo.src = this._image;
+        title.textContent = this._name;
+        photo.alt = this._name;
       }
     
 
