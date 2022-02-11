4d763b48922484c9354589bcc406a229c8147bd6
Sergio Ferrer <73060595+sergiofrrg92@users.noreply.github.com>
2022-02-11 09:35:47 +0000

    feature: methods to popupwithform

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -1,5 +1,28 @@
 class PopupWithForm extends Popup {
-    constructor(popupSelector) {
+    constructor(popupSelector, submitter) {
+        this._submitter = submitter;
         super(popupSelector);
+    }
+
+    _getInputValues() {
+        const inputValues = {};
+        const inputs = Array.from(this._popup.querySelectorAll(".form__text-input"));
+
+        inputs.forEach((input) => {
+            inputValues[input.getAttribute('name')] = input.value;
+        });
+
+        return inputValues;
+    }
+
+    setEventListeners() {
+        //TODO LAST THING I DID
+        //add click on close button and submit event handler
+        super.setEventListeners();
+    }
+
+    close() {
+        //clear values when closing.
+        super.close();
     }
 }
