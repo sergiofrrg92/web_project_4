05cc0bc242bbce32312596c38bdefaa46fb96b4b
Sergio Ferrer <73060595+sergiofrrg92@users.noreply.github.com>
2022-02-28 21:33:39 +0000

    moved files to where they belonged

--- src/page/index.js
+++ src/page/index.js
@@ -1,12 +1,12 @@
-import "./styles/index.css";
+import "./index.css";
 
-import { FormValidator } from "./components/FormValidator.js";
-import { Card } from "./components/Card.js";
-import { PopupWithForm } from "./components/PopupWithForm.js";
-import { PopupWithImage } from "./components/PopupWithImage.js";
-import { UserInfo } from "./components/UserInfo.js"
-import { Section } from "./components/Section.js";
-import { Api } from "./components/Api.js"
+import { FormValidator } from "../components/FormValidator.js";
+import { Card } from "../components/Card.js";
+import { PopupWithForm } from "../components/PopupWithForm.js";
+import { PopupWithImage } from "../components/PopupWithImage.js";
+import { UserInfo } from "../components/UserInfo.js"
+import { Section } from "../components/Section.js";
+import { Api } from "../components/Api.js"
 
 //TODO how to use it on PopupForm when opening it, to retrieve the info.
 const userSelectors = {
